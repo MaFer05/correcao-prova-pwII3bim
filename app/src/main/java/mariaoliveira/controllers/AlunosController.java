930c36d64784029466b7dd6dc0b22834b0826d6e
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2022-10-19 17:48:35 +0000

    Delete

--- app/src/main/java/mariaoliveira/controllers/AlunosController.java
+++ app/src/main/java/mariaoliveira/controllers/AlunosController.java
@@ -52,4 +52,18 @@ public class AlunosController {
         alunosRepo.save(aluno.get());
         return "redirect:/alunos/list";
     }
+
+    @RequestMapping("delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        Optional<Aluno> aluno = alunosRepo.findById(id);
+        model.addAttribute("aluno", aluno.get());
+        return "/alunos/delete";
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public String saveDelete(@RequestParam("id") int id) {
+        alunosRepo.deleteById(id);
+        return "redirect:/alunos/list";
+
+    }
 }
